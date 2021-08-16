50d008963dc72147bccae3da1ac9aa643848d3b6
zinstack625 <klimanov625@outlook.com>
2021-08-16 21:07:54 +0300

    ocean: move led and button info to general overlay

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ $(call inherit-product, vendor/motorola/ocean/ocean-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
